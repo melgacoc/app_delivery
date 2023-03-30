b2e5edd5c51c9192c9f0ffce3b7ec199d9331ba1
Cláudio <claudio19a@hotmail.com>
2023-03-30 18:10:38 -0300

    using provider

--- front-end/src/components/productsTable.jsx
+++ front-end/src/components/productsTable.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../styles/productCard.css';
 
 function ProductsTable() {
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (value) => {
+    setCart(...cart, value);
+    console.log(cart);
+  };
+
   return (
-    <div>
-      <h1>Products Table</h1>
+    <div
+      className="container"
+      key={ id }
+    >
+      <h3>{id}</h3>
+      <p>{ name }</p>
+      <img src={ urlImage } alt={ name } />
+      <p>
+        R$:
+        {price}
+      </p>
+      <button
+        type="button"
+        id="addButton"
+        name="addbutton"
+        value={ id }
+        onClick={ ({ target }) => handleAddToCart(target.value) }
+      >
+        +
+      </button>
+      <input
+        type="number"
+        name="quantity"
+        id="quantity"
+        disabled="true"
+      />
+      <button>
+        -
+      </button>
     </div>
   );
 }
--- front-end/src/pages/shopping.jsx
+++ front-end/src/pages/shopping.jsx
@@ -1,13 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import ClientHeader from '../components/clientHeader';
 import ProductsTable from '../components/productsTable';
+import Context from '../context/context';
 
 function Shopping() {
+  const { products, setProducts } = useContext(Context);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('http://localhost:3001/products');
+      const data = await response.json();
+      console.log(data);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <ClientHeader />
-      <ProductsTable />
+      { products.map((product) => (
+        <ProductsTable key={ product.id } />
+      ))}
     </div>
   );
 }
