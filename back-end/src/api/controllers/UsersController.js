35d36a747a700bb21ae40bfa29d5fe1db712dbe1
talitasaez <tltsaez@gmail.com>
2023-03-30 15:22:20 -0300

    feat:/create token

--- back-end/src/api/controllers/UsersController.js
+++ back-end/src/api/controllers/UsersController.js
@@ -1,17 +1,29 @@
+const fs = require('fs-extra');
+const jwt = require('jsonwebtoken');
 const usersService = require('../services/UsersService');
+
+const secret = fs.readFileSync('jwt.evaluation.key');
+
+const validateLogin = (user) => {
+ const payload = { name: user.name, email: user.email, role: user.role };
+ const token = jwt.sign(payload, secret);
+ return token;
+};
 
 const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await usersService.login(email, password);
-  if (!user) return res.status(404).json({ message: 'Not Found' }); 
-  return res.status(200).json(user);
+  if (!user) return res.status(404).json({ message: 'Not Found' });
+  const token = validateLogin(user);
+  return res.status(200).json({ ...user, token });
 };
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
   const user = await usersService.register(name, email, password);
   if (user === -1) return res.status(409).json({ message: 'Name or e-mail conflict' });
-  return res.status(201).json({});
+  const token = validateLogin(user);
+  return res.status(201).json(token);
 };
 
 module.exports = {
