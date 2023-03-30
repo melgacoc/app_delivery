c598050b099b85e70c6e93a81a1f26705578c8ac
Matheus Diógenes <mathdiogenes@gmail.com>
2023-03-30 16:21:58 -0300

    fix: price decimal type

--- back-end/src/database/migrations/20230328192217-createProducts.js
+++ back-end/src/database/migrations/20230328192217-createProducts.js
@@ -14,7 +14,7 @@ module.exports = {
         allowNull: false,
       },
       price: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.DECIMAL,
         allowNull: false,
       },
       urlImage: {
--- back-end/src/database/models/ProductModel.js
+++ back-end/src/database/models/ProductModel.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   const Product = sequelize.define('Product', {
     id: { type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
     name: { type: DataTypes.STRING, allowNull: false },
-    price: { type: DataTypes.INTEGER, allowNull: false },
+    price: { type: DataTypes.DECIMAL, allowNull: false },
     urlImage: { type: DataTypes.STRING, allowNull: false },
   }, {
     timestamps: false,
